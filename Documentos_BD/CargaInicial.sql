ae0179a2dc06ee8479d98c9c4004f5bc699899a9
David <david0406p2009@hotmail.com>
2020-05-30 01:10:42 -0600

    Base de datos 4.0
    Base de datos preliminar hasta que George de la de él.

--- Documentos_BD/CargaInicial.sql
+++ Documentos_BD/CargaInicial.sql
@@ -3,8 +3,8 @@ use restaurante;
 -- ============================================ 
 -- USUARIO 
 -- ============================================ 
-insert into usuario values ('david@gmail.com','123','David','Aguilar Rojas', 22556688);
-insert into usuario values ('ian@gmail.com','123','Ian','Mora Rodrigez', 22556688);
+insert into usuario values ('david@gmail.com','pulplix','123',1);
+insert into usuario values ('ian@gmail.com','ianMora666','123',1);
 select *from usuario;
 
 -- ============================================ 
@@ -27,15 +27,6 @@ insert into platillo(nombre_platillo,id_categoria,descripcion,precio)  values ('
 select * from platillo order by 1;
 
 -- ============================================ 
--- OPCIONES 
--- ============================================
-select * from opcion;
-insert into opcion (nombre_platillo,nombre,tipo,precio) values ('Alfajor','cantidad',1,1);
-insert into opcion (nombre_platillo,nombre,tipo,precio) values ('Alfajor','Más Azucar',2,100);
-insert into opcion (nombre_platillo,nombre,tipo,precio) values ('Alfajor','Extra Dulce de leche',2,500);
-
-
--- ============================================ 
 -- UBICACION 
 -- ============================================
 insert into ubicacion values('Hatillo 5 detras de los multifamiliares casa #14','San Jose','San Jose',10110);
@@ -54,19 +45,31 @@ select * from orden;
 -- LA CANTIDAD NO DEBERIA DE ESTAR AQUI COMO UN CAMPO EN LA TABLA SINO COMO UNA OPCION
 insert into detalle  (id_orden,nombre_platillo,cantidad,total) values (1,'Alfajor',1,1100);
 select * from detalle;
+-- ============================================ 
+-- ADICIONAL 
+-- ============================================
+insert into adicional (nombre_platillo,nombre,tipo) values ('Alfajor','Extras',1);
+select * from adicional;
 
 
+-- ============================================ 
+-- OPCIONES 
+-- ============================================
+insert into opcion  (id_adicional,nombre,precio)  values (1,'Más Azucar',100);
+insert into opcion  (id_adicional,nombre,precio)  values (1,'Extra Dulce de leche',500);
+select * from opcion;
 
 -- ============================================ 
 -- SELECCIONADA 
 -- ============================================
 insert into seleccionada values (1,1);
 insert into seleccionada values (1,2);
-insert into seleccionada values (1,3);
+select * from seleccionada;
 
-select us.nombre Usuario, u.provincia Provincia, od.id_orden Num_Orden, c.nombre Categoria, p.nombre_platillo Platillo, o.nombre Opcion from 
-seleccionada s, opcion o, detalle d, orden od, ubicacion u, platillo p, categoria c, usuario us
-where s.id_opcion = o.id_opcion and
+select us.username Usuario, u.provincia Provincia, od.id_orden Num_Orden, c.nombre Categoria, p.nombre_platillo Platillo, ad.nombre Adicional, o.nombre Opcion from 
+seleccionada s, opcion o, detalle d, orden od, ubicacion u, platillo p, categoria c, usuario us, adicional ad
+where  s.id_opcion = o.id_opcion and
+ad.id_adicional = o.id_adicional and
 s.id_detalle = d.id_detalle and
 d.nombre_platillo = p.nombre_platillo and
 p.id_categoria = c.id_categoria and
